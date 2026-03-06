be4fdbf6247377ad1723ae776773a61e20eb3cb7
abhinayareddyb <abhinaya@campx.in>
2026-03-06 15:28:18 +0530

    login flow

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import RegisterPage from "./pages/Auth/RegisterPage";
 import { Toaster } from "sonner";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "./pages/Auth/LoginPage";
 function App() {
   return (
     <>
       <Toaster position="top-center" richColors />
-      <RegisterPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/register" />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- Frontend/src/pages/Auth/LoginPage.tsx
+++ Frontend/src/pages/Auth/LoginPage.tsx
@@ -1,9 +1,9 @@
-import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
-import { Target } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import { login } from "./service";
+import { toast } from "sonner";
+import { Target } from "lucide-react";
 import {
   Form,
   FormControl,
@@ -12,77 +12,52 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { register } from "./service";
-import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 const schema = yup.object().shape({
-  name: yup.string().required("Name Cannot Be Empty"),
   email: yup
     .string()
-    .required("Email Cannot Be Empty")
-    .email("Enter Valid Email"),
-  password: yup
-    .string()
-    .min(6, "Password Needs To Be 6 Or More Than 6 Characters")
-    .required("Password is required"),
+    .email("Enter Valid Email")
+    .required("Email Cannot Be Empty"),
+  password: yup.string().required("Password Is Required"),
 });
-
-const RegisterPage = () => {
-  //useform
+export const LoginPage = () => {
   const form = useForm({
     resolver: yupResolver(schema),
-    defaultValues: { name: "", email: "", password: "" },
+    defaultValues: { email: "", password: "" },
   });
-  //onSubmit
+
   const onSubmit = async (data: yup.InferType<typeof schema>) => {
     try {
-      const response = await register(data);
+      const response = await login(data);
       console.log(response);
-      console.log("resetting");
-      form.reset({ name: "", email: "", password: "" });
-      toast.success("Account Created Sucessfully");
+      form.reset({ email: "", password: "" });
+      toast.success("You Are Sucessfully Logged In");
     } catch (error: any) {
       toast.error(error?.response?.data?.message ?? "Something went wrong!");
     }
   };
-  //Ui
   return (
     <div className="min-h-screen w-full bg-[#0A0F1E] flex items-center justify-center p-4">
       <div className="flex flex-col gap-3 md:gap-5 w-full md:w-120 bg-[#1A1F2E] p-5 md:p-8 rounded-2xl">
-        <div className="flex flex-col gap-2 items-center">
-          <div className="w-12 h-12 md:w-16 md:h-16 rounded-2xl bg-gradient-to-br from-purple-600 to-cyan-500 flex items-center justify-center">
-            <Target className="w-6 h-6 md:w-8 md:h-8 text-white" />
+        <div className="flex flex-col items-center gap-2">
+          <div className="h-12 w-12 md:w-16 md:h-16 rounded-2xl bg-gradient-to-br from-purple-600 to-cyan-500 flex items-center justify-center">
+            <Target className="w-6 h-6 md:w-8 md:h-8 text-white flex" />
           </div>
           <h1 className="text-xl md:text-[28px] font-medium bg-gradient-to-br from-purple-400 to-cyan-400 bg-clip-text text-transparent">
             DayQuest
           </h1>
           <p className="text-sm md:text-[16px] text-slate-400">
-            Start your journey today
+            Login to continue your journey
           </p>
         </div>
-
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
             className="flex flex-col gap-3 md:gap-4"
           >
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-xs md:text-sm font-medium text-slate-300">
-                    Name
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter Your Name" {...field} />
-                  </FormControl>
-                  <FormMessage className="text-xs" />
-                </FormItem>
-              )}
-            />
-
             <FormField
               control={form.control}
               name="email"
@@ -92,54 +67,47 @@ const RegisterPage = () => {
                     Email
                   </FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter Your Email Id" {...field} />
+                    <Input placeholder="Enter Your Email" {...field} />
                   </FormControl>
                   <FormMessage className="text-xs" />
                 </FormItem>
               )}
-            />
+            ></FormField>
             <FormField
-              control={form.control}
               name="password"
+              control={form.control}
               render={({ field }) => (
                 <FormItem>
                   <FormLabel className="text-xs md:text-sm font-medium text-slate-300">
                     Password
                   </FormLabel>
                   <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Enter Your Password"
-                      {...field}
-                    />
+                    <Input placeholder="Enter Your Password" {...field} />
                   </FormControl>
                   <FormMessage className="text-xs" />
                 </FormItem>
               )}
-            />
-
+            ></FormField>{" "}
             <Button
               type="submit"
               variant="ghost"
               className="w-full bg-gradient-to-r from-purple-600 to-cyan-500 cursor-pointer rounded-lg text-white font-semibold py-2 md:py-3 text-sm md:text-base hover:opacity-90 transition-opacity"
             >
-              Create Account
+              Login
             </Button>
           </form>
         </Form>
 
         <p className="text-sm text-slate-400 flex items-center justify-center gap-1">
-          Already Have Account?
+          Don't have an account?{" "}
           <Link
-            to="/login"
+            to="/register"
             className="text-sm text-purple-400 cursor-pointer hover:text-purple-300"
           >
-            Login
+            Register Here
           </Link>
         </p>
       </div>
     </div>
   );
 };
-
-export default RegisterPage;
